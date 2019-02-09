3f93d8be65e170ad2d1d6b8fcc87a7ec34f58080
kmietotwy <kmietotwy@gmail.com>
2019-02-09 11:07:40 -1000

    planets exercise

--- exercises.js
+++ exercises.js
@@ -64,6 +64,12 @@ Console log the variable
 Console log "Earth" using the index position
 Console log "Uranus" using the index position*/
 
+var planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune', 'Pluto'];
+
+console.log('Planets: ' + planets);
+console.log(planets[3]);
+console.log(planets[6]);
+
 /*7. Variables with Array for its value.
 Declare a variable named india and assign the value of 3 popular tourist attractions.  
 Console log the variable.*/
