8f4d63eb54d4c7e2386bae9bdb050fda91a02bd5
kmietotwy <kmietotwy@gmail.com>
2019-02-09 10:37:49 -1000

    completed 1 and 2

--- exercises.js
+++ exercises.js
@@ -9,9 +9,22 @@ Now, unleash your coding beast!
 /*1.  Variables with a String as their value.
 Declare variables named yourName, neighborName, teacherName.  Console log each variable.*/
 
+var yourName = 'Brenda';
+var neighborName = 'Dom';
+var teacherName = 'Vic';
+
+console.log(yourName, neighborName, teacherName);
+
+
 /*2. Variable with a Number as their value.
 Declare variables named numberOfFloorsBurjKhalifa, TokyoOlympicsYear. Please assign the actual data for each variable.
 Console log each variable.*/
+
+var numberOfFloorsBurjKhalifa = 163;
+var TokyoOlympicsYear = 2020;
+
+console.log('Number of Floors in Burj Khalifa: ' + numberOfFloorsBurjKhalifa);
+console.log('Tokyo Olympics Year: ' + TokyoOlympicsYear);
 
 /*3. Variables with Boolean values
 Declare variables named lovesPrepClass, eatsDonuts, drinksGin.  Console log each variable.*/
